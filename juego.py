09cc642181b5d80a2d575c311dc50d5696e8f838
NicolasHtk <Nicolas1325@hotmail.com>
2026-08-06 20:26:14 -0500

    Agrego mi juego ya creado xd

---
+++ juego.py
@@ -0,0 +1,7 @@
+print("Bienvenido al juego de adivinanza de números!")
+
+print("Estoy pensando en un número entre 1 y 100.")
+print("¿Puedes adivinar cuál es?")
+
+print("Tienes 10 intentos para adivinar el número correcto.")
+input("Presiona Enter para comenzar...")
